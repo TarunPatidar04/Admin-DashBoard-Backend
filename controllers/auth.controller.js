69483c0f51beea77903cb5129e8fb70da2c9afbd
TarunPatidar04 <tarun.coderfarm@gmail.com>
2025-12-18 11:56:11 +0530

    feat: enhance registration logic and improve response messages in auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import { User } from "../models/user.model.js";
+
 export const home = async (req, res) => {
   try {
     res.status(200).send("Hello Worlddddddddddddddddddddddddd!");
@@ -6,21 +8,36 @@ export const home = async (req, res) => {
   }
 };
 
-
 export const getRegister = async (req, res) => {
-  try {    
+  try {
     res.status(200).send("Hello Register!");
   } catch (error) {
     res.status(500).send({ message: "Internal Server Error" });
   }
-}
+};
 
 export const postRegister = async (req, res) => {
   try {
-    console.log(req.body);
+    const { username, email, phone, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).send({ message: "User already exists" });
+    }
     
-    res.status(200).send("Hello Register!");
+
+    const newUser = await User.create({
+      username,
+      email,
+      phone,
+      password,
+    });
+
+    res
+      .status(201)
+      .send({ message: "User registered successfully", user: newUser });
+
   } catch (error) {
     res.status(500).send({ message: "Internal Server Error" });
   }
-}
+};
