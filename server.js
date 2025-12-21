d1e2b4fcf51479ff7f6e120ba7f91d46a62d04fb
TarunPatidar04 <tarun.coderfarm@gmail.com>
2025-12-21 15:22:55 +0530

    refactor: standardize CORS configuration and improve code formatting in server.js

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
-import authRouter from './routes/auth.route.js';
-import contactRouter from './routes/contact.route.js';
-import { connectDB } from './utils/db.js';
-import { errorMiddleware } from './middlewares/error.middleware.js';
-import cors from 'cors';
+import authRouter from "./routes/auth.route.js";
+import contactRouter from "./routes/contact.route.js";
+import { connectDB } from "./utils/db.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
+import cors from "cors";
 
 // connectDB();
-app.use(cors(
-  {
-    origin: 'http://localhost:5173/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  }
-));
-
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 // middleware
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 app.use(express.json());
-app.use('/api/auth', authRouter);
-app.use('/api/contact', contactRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/contact", contactRouter);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-}
-)
+});
